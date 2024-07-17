76d291d19a6a3a686eba59cbcb13219fdabb2dde
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-07-17 13:48:16 -0300

    feat: react filmes-online component footer policy use

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className={style.footer}>
       <p>
         <a href="assets/Politica-de-Privacidade.pdf">Politica de privacidade</a> -{' '}
-        <a href="/Termos-e-condicoes.pdf">Termos de uso</a>
+        <a href="assets/Termos-e-condicoes.pdf">Termos de uso</a>
       </p>
       <p>Desenvolvido por Hydrah Tecnologia - todos os direitos reservados</p>
     </footer>
