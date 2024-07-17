2671b98ff9403f4cded0b8458f8a3216ae3bb5ea
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-07-17 14:30:16 -0300

    feat: react filmes-online component navbar imput, style, align

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,20 +2,27 @@
 
 import { useState } from 'react';
 import Link from 'next/link';
-//import { useRouter, useParams, usePathname } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi';
 
 import style from './style.min.module.css';
 
 const NavBar = () => {
   const [search, setSearch] = useState('');
-  //const navigate = useNavigate();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleSubmit = (e: any) => {
     e.preventDefault();
 
     if (!search) return;
+
+    const sp = new URLSearchParams(searchParams);
+    sp.set('q', search);
+
+    router.push(`/search?${sp.toString()}`);
 
     //navigate(`/search?q=${search}`, { replace: true });
     setSearch('');
